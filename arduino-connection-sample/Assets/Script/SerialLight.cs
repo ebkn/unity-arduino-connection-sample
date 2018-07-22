01071242602aa106e5e18f5ca01252c19388fa8b
ebkn <ktennis.mqekr12@gmail.com>
2018-07-22 20:01:42 +0900

    init commit

---
+++ arduino-connection-sample/Assets/Script/SerialLight.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class SerialLight : MonoBehaviour {
+
+	public SerialHandler serialHandler;
+	public Text text;
+
+	void Start () {
+		serialHandler.OnDataReceived += OnDataReceived;
+	}
+
+	void OnDataReceived(string message) {
+		try {
+			text.text = message;
+		} catch (System.Exception e) {
+			Debug.LogWarning(e.Message);
+		}
+	}
+}
---
+++ unity-arduino-test/unity-arduino-test.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int v = analogRead(0);
+  Serial.println(v);
+  delay(1000);
+}
